e290987c952701eaedbfbb45f224c9abde0ae0fe
zzzcolcol <zzzcolcol19@naver.com>
2025-04-04 15:52:03 +0900

    update hello

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +10,13 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@RestController
+	class Helloworld {
+        @GetMapping("/")
+        public String greet() {
+                return "Hello!";
+        }
+}
 
 }
