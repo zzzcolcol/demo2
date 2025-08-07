170977139bbd718b5db516feeba0407bfcc2b10d
zzzcolcol <zzzcolcol19@naver.com>
2025-08-07 16:40:51 +0900

    trigger error code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-// --- [추가 1] Logger를 사용하기 위한 import 추가 ---
+// Logger를 사용하기 위한 import 추가
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,18 +19,31 @@ public class DemoApplication {
     @RestController
     class HelloController {
 
-        // --- [추가 2] 로그를 남기기 위한 Logger 객체 생성 ---
+        // 로그를 남기기 위한 Logger 객체 생성
         private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 
         @GetMapping("/")
         public String hello() {
-            // --- [추가 3] 원하는 로그 출력 코드 삽입 ---
-            // 이 로그들이 이제 kubectl logs 에 나타날 것입니다.
-            System.out.println(">>> System.out.println: / endpoint was called!");
-            log.info(">>> log.info: Hello endpoint processing request.");
-            log.debug(">>> log.debug: This is a debug message."); // DEBUG 레벨 로그
-
+            // 기존에 잘 동작하던 엔드포인트
+            log.info(">>> log.info: / endpoint was called successfully.");
             return "Hello! world!!!!";
+        }
+        
+        // --- [추가] 에러 발생을 위한 새로운 테스트 엔드포인트 ---
+        @GetMapping("/error")
+        public String makeError() {
+            log.info(">>> log.info: /error endpoint called. Intentionally causing an error...");
+            try {
+                // 고의로 NullPointerException 발생시키기
+                String nullString = null;
+                nullString.length(); // 이 라인에서 에러가 발생합니다.
+            } catch (Exception e) {
+                // 에러를 로그에 기록합니다. 스택 트레이스가 포함됩니다.
+                log.error(">>> log.error: An exception occurred!", e);
+                // 에러를 다시 던져서 Spring Boot의 기본 에러 핸들러가 처리하도록 합니다.
+                throw e;
+            }
+            return "You should not see this message.";
         }
     }
 }
